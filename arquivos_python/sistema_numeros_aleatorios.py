5cb1d6ae1a26546e6a7c1597666cbd12a1a59481
ricardo <ricardo.s.cruz7@aluno.senai.br>
2025-04-07 14:27:22 -0300

    parte faltando
    a parte que esta faltando

---
+++ arquivos_python/sistema_numeros_aleatorios.py
@@ -0,0 +1,5 @@
+   # Loop até o usuário acertar o número
+    while acertou == False:
+        # O usuário tenta adivinhar
+        palpite = int(input("Digite seu palpite: "))
+        tentativas += 1
